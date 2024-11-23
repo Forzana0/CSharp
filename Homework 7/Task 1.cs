09c72698b26a1741f2e18ded80746bb3712a2f44
Forzana0 <157957433+Forzana0@users.noreply.github.com>
2024-11-23 09:02:02 +0200

    Add files via upload

---
+++ Homework 7/Task 1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+public class Journal
+{
+    private string name;
+    private int yearFounded;
+    private string description;
+    private string contactPhone;
+    private string email;
+    private int numberOfEmployees;
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public int YearFounded
+    {
+        get { return yearFounded; }
+        set { yearFounded = value; }
+    }
+
+    public string Description
+    {
+        get { return description; }
+        set { description = value; }
+    }
+
+    public string ContactPhone
+    {
+        get { return contactPhone; }
+        set { contactPhone = value; }
+    }
+
+    public string Email
+    {
+        get { return email; }
+        set { email = value; }
+    }
+
+    public int NumberOfEmployees
+    {
+        get { return numberOfEmployees; }
+        set { numberOfEmployees = value; }
+    }
+
+    public void EnterJournalData(string name, int yearFounded, string description, string contactPhone, string email, int numberOfEmployees)
+    {
+        this.name = name;
+        this.yearFounded = yearFounded;
+        this.description = description;
+        this.contactPhone = contactPhone;
+        this.email = email;
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    public void DisplayJournalData()
+    {
+        Console.WriteLine($"Journal Name: {name}");
+        Console.WriteLine($"Year Founded: {yearFounded}");
+        Console.WriteLine($"Description: {description}");
+        Console.WriteLine($"Contact Phone: {contactPhone}");
+        Console.WriteLine($"Email: {email}");
+        Console.WriteLine($"Number of Employees: {numberOfEmployees}");
+    }
+
+    public static Journal operator +(Journal journal, int employeesToAdd)
+    {
+        journal.numberOfEmployees += employeesToAdd;
+        return journal;
+    }
+
+    public static Journal operator -(Journal journal, int employeesToRemove)
+    {
+        journal.numberOfEmployees -= employeesToRemove;
+        return journal;
+    }
+
+    public static bool operator ==(Journal journal1, Journal journal2)
+    {
+        return journal1.numberOfEmployees == journal2.numberOfEmployees;
+    }
+
+    public static bool operator !=(Journal journal1, Journal journal2)
+    {
+        return journal1.numberOfEmployees != journal2.numberOfEmployees;
+    }
+
+    public static bool operator <(Journal journal1, Journal journal2)
+    {
+        return journal1.numberOfEmployees < journal2.numberOfEmployees;
+    }
+
+    public static bool operator >(Journal journal1, Journal journal2)
+    {
+        return journal1.numberOfEmployees > journal2.numberOfEmployees;
+    }
+
+    public static bool operator <=(Journal journal1, Journal journal2)
+    {
+        return journal1.numberOfEmployees <= journal2.numberOfEmployees;
+    }
+
+    public static bool operator >=(Journal journal1, Journal journal2)
+    {
+        return journal1.numberOfEmployees >= journal2.numberOfEmployees;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Journal)
+        {
+            var otherJournal = (Journal)obj;
+            return this.numberOfEmployees == otherJournal.numberOfEmployees;
+        }
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return numberOfEmployees.GetHashCode();
+    }
+}
---
+++ Homework 7/Task 2.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+public class Store
+{
+    private string storeName;
+    private string storeAddress;
+    private string storeDescription;
+    private string contactPhone;
+    private string email;
+    private double storeArea;
+    public string StoreName
+    {
+        get { return storeName; }
+        set { storeName = value; }
+    }
+
+    public string StoreAddress
+    {
+        get { return storeAddress; }
+        set { storeAddress = value; }
+    }
+
+    public string StoreDescription
+    {
+        get { return storeDescription; }
+        set { storeDescription = value; }
+    }
+
+    public string ContactPhone
+    {
+        get { return contactPhone; }
+        set { contactPhone = value; }
+    }
+
+    public string Email
+    {
+        get { return email; }
+        set { email = value; }
+    }
+
+    public double StoreArea
+    {
+        get { return storeArea; }
+        set { storeArea = value; }
+    }
+
+    public void EnterStoreData(string storeName, string storeAddress, string storeDescription, string contactPhone, string email, double storeArea)
+    {
+        this.storeName = storeName;
+        this.storeAddress = storeAddress;
+        this.storeDescription = storeDescription;
+        this.contactPhone = contactPhone;
+        this.email = email;
+        this.storeArea = storeArea;
+    }
+
+    public void DisplayStoreData()
+    {
+        Console.WriteLine($"Store Name: {storeName}");
+        Console.WriteLine($"Store Address: {storeAddress}");
+        Console.WriteLine($"Description: {storeDescription}");
+        Console.WriteLine($"Contact Phone: {contactPhone}");
+        Console.WriteLine($"Email: {email}");
+        Console.WriteLine($"Store Area: {storeArea} square meters");
+    }
+
+    public static Store operator +(Store store, double areaToAdd)
+    {
+        store.storeArea += areaToAdd;
+        return store;
+    }
+
+    public static Store operator -(Store store, double areaToRemove)
+    {
+        store.storeArea -= areaToRemove;
+        return store;
+    }
+
+    public static bool operator ==(Store store1, Store store2)
+    {
+        return store1.storeArea == store2.storeArea;
+    }
+
+    public static bool operator !=(Store store1, Store store2)
+    {
+        return store1.storeArea != store2.storeArea;
+    }
+
+    public static bool operator <(Store store1, Store store2)
+    {
+        return store1.storeArea < store2.storeArea;
+    }
+
+    public static bool operator >(Store store1, Store store2)
+    {
+        return store1.storeArea > store2.storeArea;
+    }
+
+    public static bool operator <=(Store store1, Store store2)
+    {
+        return store1.storeArea <= store2.storeArea;
+    }
+
+    public static bool operator >=(Store store1, Store store2)
+    {
+        return store1.storeArea >= store2.storeArea;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Store)
+        {
+            var otherStore = (Store)obj;
+            return this.storeArea == otherStore.storeArea;
+        }
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return storeArea.GetHashCode();
+    }
+}
